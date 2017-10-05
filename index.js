75847d5a7ea5992801a92b81a4480205e7791500
Andy Striph <ajstriph@me.com>
2017-10-05 21:38:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
-var updateObjectWithKeyAndValue = function (object, key, value){
+var destructivelyUpdateObjectWithKeyAndValue = function (object, key, value){
 object[key] = value;
   return object;
 }
