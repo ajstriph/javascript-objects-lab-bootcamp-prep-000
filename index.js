f149a9cca0e0a165cfaa632a05310d154313d894
Andy Striph <ajstriph@me.com>
2017-10-05 20:50:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,4 @@ var obj = {prop: 1};
 object['prop 2'] = 2;
   return updateObjectWithKeyAndValue;
 }
-updateObjectWithKeyAndValue = function (object, key, value){
-var obj = {prop: 1};
-  return updateObjectWithKeyAndValue{obj, ['prop 2'] 2};
-}
+
