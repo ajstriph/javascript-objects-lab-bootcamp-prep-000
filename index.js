134f0fa92246e44c6ae59ee8a5f40c0a32fc5f97
Andy Striph <ajstriph@me.com>
2017-10-05 20:33:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = {};
 var updateObjectWithKeyAndValue = function (object, key, value){
-object[key]="value";
+var obj = {prop: 1};
+object[key] = "value";
   return updateObjectWithKeyAndValue (obj, 'prop 2', 2);
 }
