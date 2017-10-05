ad0d09acd033f76f48d07947170b88740079153a
Andy Striph <ajstriph@me.com>
2017-10-05 20:53:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ object['prop 2'] = 2;
   return updateObjectWithKeyAndValue;
 }
 var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value){
+  var obj = {prop: 1};
   obj['prop 2']=2;
   return object;
 }
